3b50cdcc6475c75ef182a22c205b7e8fddac7e9e
Anita F. McFarland <anitam2345@yahoo.com>
2018-10-07 20:03:55 -0700

    git commit-m"initial update"

---
+++ assets/javascript/game.js
@@ -0,0 +1,84 @@
+// var random_result;
+// var lost = 0;
+// var win =0;
+// var previous = 0;
+
+// var resetAndStart = function(){
+//     $(".crystals").empty();
+
+//      var images = [
+//     //     // "http:" copy and paste 4 images here
+//         img ="assets/images/amethyst.jpg", alt="amethyst"
+    //     img ="assets/images/ruby.jpg", alt="ruby"
+    //     img ="/sapphire.jpg", alt="sapphire"
+    //     img ="/topaz.jpg", alt="topaz"
+        
+        
+//   ]        
+// }
+// {
+
+   
+// random_result = Math.floor(Math.random()* 101) + 19;
+
+// $("#result").html("Random Result:" + random_result);
+// for(var i=o; i< 4; i++){
+
+    // var random = Math.floor(Marth.random()* 11) +1;
+
+    //  var crystal = $("<div>");
+    //     crystal.attr
+    //  ("class:" ,"crystal");
+    //         "data-random": random  
+    //      } );
+
+    //     crystal.css({
+    //         "background-image":"url(" + images[i] + ")",
+    //         "background:size":"cover"
+           
+
+        // })
+//  $(".crystals").append(crystal);       
+// }
+// shows numbers to check
+//  crystal.html(random);
+
+// $("#previous").html("Total Score:" + previous);{
+ 
+// resetAndStart();
+//  }
+//     $(".crystal").on("click", function (){
+//     var result;
+     
+
+// // $(document).on("click","crystal", function (){
+//     $(document).ready(function(){
+//         $("crystal").click(function(){
+//             $(this).hide(200)
+//             ;
+//         })
+    
+//     var num =parseInt$(this).attr}("data-random");
+//     previous += num;
+    
+// $("#previous").html("Total score: " + previous);
+//     if(previous > random_result){ 
+//         lost--;
+//         $("#lost").html("You Lost: " + lost);
+//         previous = 0;
+    
+//     resetAndStart();
+//     }
+//     else if(previous === random_result) 
+//     win++;
+//     $("#win").html("You Win!:" + win);
+//     previous = 0;
+
+//     resetAndStart();
+
+    
+        
+
+
+
+// } 
