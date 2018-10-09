b24850276a644ec0ec53376164a845f1b79c5d98
Anita F. McFarland <anitam2345@yahoo.com>
2018-10-08 20:47:23 -0700

    git commit-m"mon update"

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,84 +1,71 @@
-// var random_result;
-// var lost = 0;
-// var win =0;
-// var previous = 0;
+ var random_result;
+ var lost = 0;
+ var win =0;
+ var previous = 0;
 
-// var resetAndStart = function(){
-//     $(".crystals").empty();
+ var resetAndStart = function(){
+     $(".crystal").empty();
 
-//      var images = [
-//     //     // "http:" copy and paste 4 images here
-//         img ="assets/images/amethyst.jpg", alt="amethyst"
-    //     img ="assets/images/ruby.jpg", alt="ruby"
-    //     img ="/sapphire.jpg", alt="sapphire"
-    //     img ="/topaz.jpg", alt="topaz"
-        
-        
-//   ]        
-// }
-// {
-
+ }
    
-// random_result = Math.floor(Math.random()* 101) + 19;
+    random_result = Math.floor(Math.random()* 101) + 19;
 
-// $("#result").html("Random Result:" + random_result);
-// for(var i=o; i< 4; i++){
+        $("#result").html("Random Result:" + random_result);
+        for(var i=o; i< 4; i++){
 
-    // var random = Math.floor(Marth.random()* 11) +1;
+        var random = Math.floor(Marth.random()* 11) +1;
 
-    //  var crystal = $("<div>");
-    //     crystal.attr
-    //  ("class:" ,"crystal");
-    //         "data-random": random  
-    //      } );
+        var crystal = $("<div>");
+         crystal.attr
+          ("class:" ,"crystal");
+            "data-random"; random  
+         } ;
+         var imageCrystal = $("<img>");
+             imageCrystal.attr("<img>");
 
-    //     crystal.css({
-    //         "background-image":"url(" + images[i] + ")",
-    //         "background:size":"cover"
-           
+       
+         $(".crystal").append(crystal);       
+        
+        //  shows numbers to check
+         crystal.html(random);
 
-        // })
-//  $(".crystals").append(crystal);       
-// }
-// shows numbers to check
-//  crystal.html(random);
-
-// $("#previous").html("Total Score:" + previous);{
+     $("#previous").html("Total Score:" + previous);{
  
-// resetAndStart();
-//  }
-//     $(".crystal").on("click", function (){
-//     var result;
+        resetAndStart();
+     }
+        $(".crystal").on("click", function (){
+         var result;
      
 
-// // $(document).on("click","crystal", function (){
-//     $(document).ready(function(){
-//         $("crystal").click(function(){
-//             $(this).hide(200)
-//             ;
-//         })
+     $(document).on("click","crystal", function (){
+         $(document).ready(function(){
+             $("crystal").click(function(){
+                 $(this).hide(200)
+                 ;
+            })
     
-//     var num =parseInt$(this).attr}("data-random");
-//     previous += num;
+        var num =parseInt$(this).attr("data-random"); {
+         previous += num;
     
-// $("#previous").html("Total score: " + previous);
-//     if(previous > random_result){ 
-//         lost--;
-//         $("#lost").html("You Lost: " + lost);
-//         previous = 0;
+    $("#previous").html("Total score: " + previous);
+         if(previous > random_result){ 
+             lost--;
+            $("#lost").html("Losses: " + lost);
+            previous = 0;
     
-//     resetAndStart();
-//     }
-//     else if(previous === random_result) 
-//     win++;
-//     $("#win").html("You Win!:" + win);
-//     previous = 0;
+        resetAndStart();
+         }
+         else if(previous === random_result) 
+         win++;
+         $("#win").html("Wins:" + win);
+         previous = 0;
 
-//     resetAndStart();
+         resetAndStart();
 
     
         
+    });
 
+    
 
-
-// } 
+    }       
